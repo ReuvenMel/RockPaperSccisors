aeece979b2d168afbe4bc3ed96ee706a83be59c1
ReuvenMel <154438638+ReuvenMel@users.noreply.github.com>
2023-12-20 21:51:37 +0000

    added rounds to the game

--- main.js
+++ main.js
@@ -1,38 +1,49 @@
-function getComputerChoice() {
-  const randomNumber = Math.floor(Math.random() * 3) + 1;
+let roundsToPlay = 5; 
+let playerScore = 0;
+let computerScore = 0;
 
-  if (randomNumber === 1) {
-    return "Paper";
-  } else if (randomNumber === 2) {
-    return "Rock";
+for (let roundNumber = 1; roundNumber <= roundsToPlay; roundNumber++) {
+  function getComputerChoice() {
+    const randomNumber = Math.floor(Math.random() * 3) + 1;
+
+    if (randomNumber === 1) {
+      return "Paper";
+    } else if (randomNumber === 2) {
+      return "Rock";
+    } else {
+      return "Scissors";
+    }
+  }
+
+  function getUserChoice() {
+    let playerChoice = prompt("Rock, Paper, or Scissors?");
+    playerChoice = playerChoice ? playerChoice.toLowerCase() : ""; 
+    return playerChoice;
+  }
+
+  const computerChoice = getComputerChoice();
+  const playerChoice = getUserChoice();
+
+  console.log("Computer chose: " + computerChoice);
+  console.log("Player chose: " + playerChoice);
+
+  if (playerChoice !== "rock" && playerChoice !== "paper" && playerChoice !== "scissors") {
+    alert("Invalid choice, please choose 'Rock', 'Paper', or 'Scissors'");
+    continue; 
+  }
+
+  if (computerChoice === playerChoice) {
+    console.log("It's a tie!");
+  } else if ((computerChoice === "Rock" && playerChoice === "Paper") ||
+             (computerChoice === "Paper" && playerChoice === "Scissors") ||
+             (computerChoice === "Scissors" && playerChoice === "Rock")) {
+    playerScore++;
+    console.log("Player Wins! Score: " + playerScore);
   } else {
-    return "Scissors";
+    computerScore++;
+    console.log("Computer Wins! Score: " + computerScore);
   }
 }
 
-function getUserChoice() {
-  const playerChoice = prompt("Rock, Paper, or Scissors?");
-  return playerChoice.toLowerCase(); 
-}
 
-const computerChoice = getComputerChoice();
-const playerChoice = getUserChoice();
-
-console.log("Computer chose: " + computerChoice);
-console.log("Player chose: " + playerChoice);
-
-if (playerChoice !== "rock" && playerChoice !== "paper" && playerChoice !== "scissors") {
-  alert("Invalid choice, please choose 'Rock, Paper, or Scissors?'")
-}
-
-if (getComputerChoice === playerChoice) {
-  console.log("It's a tie!");
-} else if (computerChoice === "Rock" && playerChoice === "Paper") {
-  console.log("Player Wins!");
-} else if (computerChoice === "Paper" && playerChoice === "Scissors") {
-  console.log("Player Wins!");
-} else if (computerChoice === "Scissors" && playerChoice === "Rock") {
-  console.log("Player Wins!");
-} else {
-  console.log("Computer Wins!");
-}
+console.log("Final Scores - Player: " + playerScore + ", Computer: " + computerScore);
